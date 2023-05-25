723cff571aee7c35eddabccd85f5ef10a221c4f8
tshering567 <tseringd334@gmail.com>
2023-05-25 15:25:37 -0400

    added a customer greeting

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String userInput = scanner.nextLine();
+        System.out.println("Hello"  + " " + userInput);
     }
 }
